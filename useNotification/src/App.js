49fb8799b993230a6003c153fc57892e310ad377
th7024 <th7024@gmail.com>
2021-02-09 00:49:45 +0900

    사용자에게 알림창 띄워주기

---
+++ useNotification/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useNotification = (title, options) => {
+  if(!("Notification" in window)){
+    return;
+  }
+  const fireNotif = () => {
+    if(Notification.permission !== "granted"){
+      Notification.requestPermission().then(permission => {
+        if(permission ==="granted"){
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else{
+      new Notification(title,options);
+    }
+    
+  }
+  return fireNotif;
+}
+
+const App = () => {
+  const triggerNotif = useNotification("Can I steal yout kimchi", {
+    body:"yey"
+  })
+  return (
+    <div className="App" style={{ height: "1000vh" }}>
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+export default App;
